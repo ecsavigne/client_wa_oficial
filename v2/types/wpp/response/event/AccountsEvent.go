c1dbcc54c9719befae0bb1127497a1662d3e6bfc
ecsavigne <ecsavigne@gmail.com>
2026-06-24 00:55:02 -0300

    add method GetEvent in AccountUpdateEvent

--- v2/types/wpp/response/event/AccountsEvent.go
+++ v2/types/wpp/response/event/AccountsEvent.go
@@ -5,6 +5,17 @@ import (
 	"github.com/ecsavigne/client_wa_oficial/v2/types/wpp/response"
 )
 
+type TYPE_ACCOUNT_UPDATE_EVENT string
+
+var (
+	ACCOUNT_DELETED, ACCOUNT_RESTRICTION, ACCOUNT_VIOLATION, AD_ACCOUNT_LINKED                 TYPE_ACCOUNT_UPDATE_EVENT
+	AUTH_INTL_PRICE_ELIGIBILITY_UPDATE, BUSINESS_PRIMARY_LOCATION_COUNTRY_UPDATE               TYPE_ACCOUNT_UPDATE_EVENT
+	DISABLED_UPDATE, MM_LITE_TERMS_SIGNED, PARTNER_ADDED                                       TYPE_ACCOUNT_UPDATE_EVENT
+	PARTNER_APP_INSTALLED, PARTNER_APP_UNINSTALLED, PARTNER_CLIENT_CERTIFICATION_STATUS_UPDATE TYPE_ACCOUNT_UPDATE_EVENT
+	PARTNER_REMOVED, VOLUME_BASED_PRICING_TIER_UPDATE, ACCOUNT_OFFBOARDED                      TYPE_ACCOUNT_UPDATE_EVENT
+	ACCOUNT_RECONNECTED                                                                        TYPE_ACCOUNT_UPDATE_EVENT
+)
+
 type AccountUpdateEvent struct {
 	*Components
 }
@@ -12,6 +23,10 @@ type AccountUpdateEvent struct {
 func (*AccountUpdateEvent) GetType() wpp.EventType { return wpp.EventTypeAccountUpdate }
 
 func (m *AccountUpdateEvent) String() string { return response.Val(m) }
+
+func (self *AccountUpdateEvent) GetEvent() string {
+	return self.GetEntry()[0].GetChange()[0].GetValue().Event
+}
 
 type AccountAlertsEvent struct {
 	*Components
