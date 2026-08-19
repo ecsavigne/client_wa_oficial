4a7dd1d1f15ae9d61a1a853c72e841f0fbb74db2
ecsavigne <ecsavigne@gmail.com>
2026-08-19 16:14:42 -0300

    up

--- v2/types/ig/response/event/MessageEvent.go
+++ v2/types/ig/response/event/MessageEvent.go
@@ -10,6 +10,7 @@ type MessageEvent struct {
 }
 
 // func (*MessageEvent) GetType() igpbv1.EventType { return igpbv1.EventTypeMessage }
+
 // func (m *MessageEvent) String() string { return response.Val(m) }
 
 type IGStatusMessageEvent struct {
